5c7976de5a967f00373917e4cbf90a4dc3968370
lucas_cabrera1 <lucas_cabrera1@hotmail.com>
2023-11-13 16:14:17 -0300

    intenta agregar una linea de compra

--- client/src/Components/FormOrdenCompra.js
+++ client/src/Components/FormOrdenCompra.js
@@ -7,7 +7,7 @@ import Input from './Common/Input'
 import Form from 'react-bootstrap/Form'
 import {RecuperarProveedores, RecuperarProductosPorProveedor, AgregarOrdenDeCompra,
   SeleccionarTodosLosProductos, SeleccionarTodosLosProveedores,
-  EstadoProveedores, EstadoProductos} from '../Features/OrdenCompraSlice.js'
+  EstadoProveedores} from '../Features/OrdenCompraSlice.js'
 import Table from 'react-bootstrap/esm/Table.js'
 
 function FormOrdenCompra() {
@@ -15,11 +15,13 @@ function FormOrdenCompra() {
   const navigate = useNavigate()
   const dispatch = useDispatch()
   const proveedores = useSelector(SeleccionarTodosLosProveedores)
-  const productos = useSelector(SeleccionarTodosLosProductos)
   const estadoproveedores = useSelector(EstadoProveedores)
-  /* const estadoproductos = useSelector(EstadoProductos)
-  const [idProveedor, setIdProveedor] = useState("") */
+  const productos = useSelector(SeleccionarTodosLosProductos)
+  const [idProveedor, setIdProveedor] = useState("")
   const {register, handleSubmit, formState : {errors}} = useForm()
+  const [producto, setProducto] = useState()
+  const [total, setTotal] = useState()
+  const [cantidad, setCantidad] = useState()
 
   useEffect(()=>{
     if (estadoproveedores==="idle"){
@@ -33,10 +35,42 @@ function FormOrdenCompra() {
     >{p.razonsocial}
   </option>))
 
+  const AgregarLineaCompra = (producto, cantidad) => {
+    console.log(cantidad)
+    console.log(producto)
+    let lineaCompra = {
+      producto: producto,
+      cantidad: cantidad,
+      subtotal : producto.preciocompra * cantidad
+    }
+    setTotal(total + lineaCompra.subtotal)
+    console.log(lineaCompra)
+    console.log(total)
+  }
+
+  const handleSubmitLC = (data) => {
+    console.log(data)
+    console.log('handle submit linea compra')
+    setCantidad(data.cantidad)
+    console.log(cantidad)
+  }
+
+  const handleSubmitOC = (data) => {
+    console.log(data)
+    console.log('handle submit orden compra')
+  }
+
   return (
     <div>
+      <Form onSubmit={handleSubmit(handleSubmitLC)}>
+        <div>
+          formulario dentro de otro formulario
+          <Button type='submit'> Enviar linea compra</Button>
+        </div>
+      </Form>
+      <br/>
       Acá voy a generar UNA orden de compra
-      <Form style={{width: '450px', border: '2px solid black'}}>
+      <Form onSubmit={handleSubmit(handleSubmitLC)} style={{border: '2px solid black'}}>
         <label>Proveedor</label>
         <Form.Select
           aria-label='Proveedor'
@@ -52,44 +86,101 @@ function FormOrdenCompra() {
         >
           {optionProveedores}
         </Form.Select>
-      </Form>
-      <Table className= 'table table-success table-bordered border-dark'>
+      
+      <Table
+        className= 'table table-success table-bordered border-dark'
+        /* onClick={(e)=> {
+          console.log(e)
+          setProducto(e.target)
+          console.log(producto)
+        }} */
+      >
         <thead>
             <tr>
-                <th>Descripción</th>
-                <th>Categoría</th>
-                <th>Proveedor</th>
-                <th>Marca</th>
-                <th>Código</th>
-                <th>Precio de Compra</th>
-                <th>Precio de Venta</th>
-                <th>Punto de Pedido</th>
-                <th>Stock</th>
+              <th>Descripción</th>
+              <th>Categoría</th>
+              <th>Proveedor</th>
+              <th>Marca</th>
+              <th>Código</th>
+              <th>Precio de Compra</th>
+              <th>Precio de Venta</th>
+              <th>Punto de Pedido</th>
+              <th>Stock</th>
+            </tr>
+        </thead>
+        <tbody>
+          {productos.map( producto => { 
+            return <tr key={producto._id} onClick={e=> {
+              e.preventDefault()
+              console.log(producto)
+              setProducto(producto)
+            }}>
+              <td>{producto.descripcion}</td>
+              <td>{producto.categoria}</td>
+              <td>{producto.proveedor}</td>
+              <td>{producto.marca}</td>
+              <td>{producto.codigo}</td>
+              <td>{producto.preciocompra}</td>
+              <td>{producto.precioventa}</td>
+              <td>{producto.puntopedido}</td>
+              <td>{producto.stock}</td>
+            </tr>
+          })}
+        </tbody>
+      </Table>
+      
+      <p>Producto seleccionado: {producto._id}</p>
+      <Input
+        type="number"
+        name="cantidad"
+        placeholder="ingrese la cantidad"
+        register = {register}
+        registerOptions={{required: true, min: 1}}
+        optionMsgErrors={{
+          required: "La cantidad es requerida",
+          min: "minimo 1 producto"
+        }}
+        errors={errors}
+      />
+      <Button
+        onClick={(e)=>{
+          e.preventDefault()
+          AgregarLineaCompra(producto, cantidad);
+          console.log(producto)
+      }}
+        >Agregar línea de compra
+      </Button>
+
+      <p>Descripción de la orden de compra al momento</p>
+      { <Table className= 'table table-success table-bordered border-dark'>
+        <thead>
+            <tr>
+              <th>Producto</th>
+              <th>Cantidad</th>
+              <th>Precio Unitario</th>
+              <th>Subtotal</th>
             </tr>
         </thead>
         <tbody>
             {
               productos.map( producto => { 
                   return <tr key={producto._id}>
-                    <td>{producto.descripcion}</td>
-                    <td>{producto.categoria}</td>
-                    <td>{producto.proveedor}</td>
-                    <td>{producto.marca}</td>
-                    <td>{producto.codigo}</td>
-                    <td>{producto.preciocompra}</td>
-                    <td>{producto.precioventa}</td>
-                    <td>{producto.puntopedido}</td>
-                    <td>{producto.stock}</td>
+                    <td>Producto</td>
+                    <td>Precio de Compra</td>
+                    <td>precio Unitario</td>
+                    <td>Subtotal</td>
                   </tr>
                 }
               )
             }
         </tbody>
-      </Table>
+      </Table>}
       <NavLink 
         onClick={e => { e.preventDefault(); navigate('/todaslasordenesdecompra')}}>
         ...Atrás
       </NavLink>
+      <Button type='submit'>Generar nueva orden de compra</Button>
+      </Form>
     </div>
   )
 }
